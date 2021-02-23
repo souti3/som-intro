69721a5e96dd2ce9cec03950fc6728fe15d69486
Marcel Soltermann <marcel.soltermann@gmail.com>
2021-02-23 21:24:29 +0100

    [ADD] added carousel slider component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CarouselComponent } from './carousel/carousel.component';
     CarouselComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
 })
 export class CarouselComponent implements OnInit {
 
-  constructor() { }
+  images = ['../../assets/images/sl1.jpg', '../../assets/images/sl2.jpg', '../../assets/images/sl3.jpg']
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 2000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+   }
 
   ngOnInit(): void {
   }
